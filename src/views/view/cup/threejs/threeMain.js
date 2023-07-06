9d681d021744dcb84742717520b816dad2989858
huamingy <2191452146@qq.com>
2023-07-06 21:12:43 +0800

    0

--- src/views/view/cup/threejs/threeMain.js
+++ src/views/view/cup/threejs/threeMain.js
@@ -1,4 +1,6 @@
 import { WebGLRenderer ,Scene,PerspectiveCamera} from "three";
+import { AmbientLight, PointLight } from "three"
+import { BoxGeometry, Color, Mesh, MeshStandardMaterial } from "three"
 
 export class ThreMain{
 
@@ -18,12 +20,18 @@ renderer.setSize(dom.offsetWidth, dom.offsetHeight, true)
 
 let scene = new  Scene()
 
-
+// scene.background = new /
 const ambientLight = new AmbientLight('rgb(255,255,255)', 0.8)
 
 scene.add(ambientLight)
-
-
+const pointLight = new PointLight(
+    'rgb(255,255,255)',
+    0.5,
+    600,
+    0.2
+  )
+  pointLight.position.set(0, 50, 100)  // 设置点光源位置
+  scene.add(pointLight)
 // 实例化相机
 let camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000)  
 
@@ -31,6 +39,31 @@ camera.position.set(50, 50, 50) // 设置相机位置
 
 // camera.lookAt(new Vector3(0, 0, 0))  // 设置相机看先中心点
 // camera.up = new Vector3(0, 1, 0)  // 设置相机自身的方向
+console.log(scene)
+
+
+
+
+
+
+
+
+
+
+const box = new Mesh(
+    new BoxGeometry(30, 20, 20),  // 设置立方体的大小
+    new MeshStandardMaterial({   // 设置材质
+      color: 'rgb(36, 172, 242)',  // 设置材质的颜色
+    //   metalness: 0.5,   // 金属度 （1 最像金属，0 最不想金属）
+    //   roughness: 0   // 粗糙度（0 最光滑，1 最粗糙）
+    })
+  )
+  box.name = 'box'  // 设置模型 name
+scene.add(box)
+
+
+
+
 
 
 
